f0d12d083e8a0862c938017409582dcc440ffc60
ewlin <ericwilliamlin@gmail.com>
2019-01-08 14:13:37 -0500

    js to ruby updates

---
+++ 4A/activity-1-solutions.js
@@ -0,0 +1,11 @@
+//Given the array: 
+var names = ["Harry", "Dev", "Nicholas", "Rachel", "Nico", "Antonio", "Octavia"];
+
+//Use an array method on names to find the index of the name "Octavia"
+
+//Solution: 
+names.indexOf("Octavia");
+
+//Using an array method, insert the name "Esther" between "Dev" and "Nicholas"
+names.splice(2, 0, "Esther");
+
--- 5A/javascript_to_ruby.rb
+++ 5A/javascript_to_ruby.rb
@@ -1,17 +1,80 @@
-#[1,2,23,41,23].each_with_index do |ele, index|
-#  puts ele
-#  puts index
-#end
+# Part 1 
+
+# In JavaScript: 
+# var myVariable = 7.4543;
+# console.log(myVariable);
+# A
+
+my_variable = 7.4543
+
+# B
+
+puts my_variable
+
+# Part 2 
+
+# In JavaScript: 
+# var mostSpokenLanguages = ['English', 'Mandarin Chinese', 'Spanish', 'Arabic', 'Hindi', 'Russian', 'Malay'];
+# mostSpokenLanguages[0];
+# mostSpokenLanguages[mostSpokenLanguages.length - 1]
+# 
+# mostSpokenLanguages.indexOf('Arabic'); 
 
 
-def everyOtherElement(array)
-  finalArray = []
+
+most_spoken_languages = ['English', 'Mandarin Chinese', 'Spanish', 'Arabic', 'Hindi', 'Russian', 'Malay']
+
+most_spoken_languages[0]
+most_spoken_languages[most_spoken_languages.length - 1]
+
+most_spoken_languages.index('Arabic')
+
+
+# Part 3 
+
+# JavaScript version: 
+# var dogProfile = {
+#   name: 'Fido',
+#   age: 3, 
+#   gender: 'male', 
+#   breed: 'mixed', 
+#   pattern: 'spotted'
+# }
+
+dog_profile = {
+  :name => 'Fido',
+  :age => 3, 
+  :gender => 'male', 
+  :breed => 'mixed', 
+  :pattern => 'spotted'
+}
+
+dog_profile[:name] # 'Fido'
+
+# Part 4 
+=begin
+function everyOtherElement(array) {
+  var finalArray = []; 
+  array.forEach(function(element, index) {
+    if (index % 2 === 1) {
+      finalArray.push(element);
+    }
+  });
+  return finalArray;
+}
+=end
+
+def every_other_element(array)
+  final_array = []
   array.each_with_index do |element, index|
     if index % 2 == 1
-      finalArray.push(element)
+      final_array.push(element)
     end
   end
-  return finalArray
+  return final_array
 end
 
-  
+
+
+
+
